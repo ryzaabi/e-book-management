763021eb3ec1d03bb3176c3f7b6595853086c128
ryzaabi <ralzaabi775@gmail.com>
2024-11-05 12:32:47 +0400

    Update order.py

--- order.py
+++ order.py
@@ -8,14 +8,17 @@ class Order:
         self.__total_price = self.calculateTotal()
 
     def calculateTotal(self):
+        #Calculates the total price after applying discounts
         subtotal = sum(ebook.getPrice() for ebook in self.__ebooks)
         return subtotal - self.__discount_applied
 
     def applyDiscount(self, discount):
+        #Applies a discount to the Order
         self.__discount_applied = discount
         self.__total_price = self.calculateTotal()
 
     def getTotalPrice(self):
+        #Returns the total price of the Order after discounts
         return self.__total_price
 
     def __str__(self):
