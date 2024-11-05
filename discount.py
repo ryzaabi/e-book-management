b3a995c53764d8f2bc6a067df46e2cf048c7058b
ryzaabi <ralzaabi775@gmail.com>
2024-11-05 12:34:33 +0400

    Update discount.py

--- discount.py
+++ discount.py
@@ -1,12 +1,14 @@
 class Discount:
     @staticmethod
     def applyLoyaltyDiscount(order):
+        #Applies a 10% loyalty discount to the Order
         discount = order.getTotalPrice() * 0.10  # 10% discount for loyalty members
         order.applyDiscount(discount)
         return discount
 
     @staticmethod
     def applyBulkDiscount(order):
-        discount = order.getTotalPrice() * 0.20  # 20% discount for 5 or more items
+        #Applies a 20% discount for bulk purchases (5 or more items)
+        discount = order.getTotalPrice() * 0.20 # 20% discount for 5 or more items
         order.applyDiscount(discount)
         return discount
