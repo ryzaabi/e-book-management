f186dcf7e3d98c3c23e46ca328beeafe680be297
ryzaabi <ralzaabi775@gmail.com>
2024-11-05 12:31:40 +0400

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -3,11 +3,14 @@ class ShoppingCart:
         self.cart_items = []
 
     def addItem(self, ebook):
+        #Adds an eBook to the shopping cart."
         self.cart_items.append(ebook)
 
     def removeItem(self, ebook):
+        #Removes an eBook from the shopping cart
         if ebook in self.cart_items:
             self.cart_items.remove(ebook)
 
     def getTotalPrice(self):
+        #Calculates and returns the total price of all eBooks in the cart.
         return sum(ebook.getPrice() for ebook in self.cart_items)
